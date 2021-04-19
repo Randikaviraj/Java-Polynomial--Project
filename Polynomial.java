e6e081898ebb5b4a8d323be016dd0e7128467134
Randikaviraj <randikavirajmax@gmail.com>
2021-04-19 10:12:56 +0530

    Add iterate and toString methods
    iterate method
    return type :  iteratable object of Tuples
    
    toString
    this returns the string of polynomial

--- Polynomial.java
+++ Polynomial.java
@@ -1,9 +1,10 @@
+import java.util.ArrayList;
 import java.util.Iterator;
 
 /**
  * Polynomial
  */
-public class Polynomial implements Iterable<Polynomial>, Comparable<Polynomial> {
+public class Polynomial implements Iterable<Tuple>, Comparable<Polynomial> {
 
     /**
      * The static Inner Node class for store exponent and coefficient of one term
@@ -40,15 +41,6 @@ public class Polynomial implements Iterable<Polynomial>, Comparable<Polynomial> 
             this.next=node;
         }
 
-        // //setter for exponent
-        // public void setExponent(int exponent){
-        //     this.exponent = exponent;
-        // }
-
-        // // setter for coefficient
-        // public void setCoefficient(double coefficient){
-        //     this.coefficient = coefficient;
-        // }
     }
 
     private Polynomial.Node headNode;
@@ -57,20 +49,26 @@ public class Polynomial implements Iterable<Polynomial>, Comparable<Polynomial> 
         
         try {
             String[] list = polynomial.split(" ");
+            
 
             if (list.length%2!=0 &&  list.length!=0) {
                 // polynomial string check
                 throw new InvalidPolynomialSyntax("Error in polynomial format");
             }
             this.headNode=new Polynomial.Node(Integer.parseInt(list[1]),Double.parseDouble(list[0]));
+            int earlyexponent=Integer.parseInt(list[1]);
             Polynomial.Node tempNode=this.headNode;
 
             for (int i = 2; i < list.length; i=i+2) {
-                tempNode.setNextNode(new Polynomial.Node(Integer.parseInt(list[i]),Double.parseDouble(list[i+1])));
+                if (earlyexponent<Integer.parseInt(list[i+1])) {
+                    throw new InvalidPolynomialSyntax("Error in polynomial format");
+                }
+                earlyexponent=Integer.parseInt(list[i+1]);
+                tempNode.setNextNode(new Polynomial.Node(Integer.parseInt(list[i+1]),Double.parseDouble(list[i])));
                 tempNode=tempNode.getNextNode();
             }
             
-        } catch (Exception e) {
+        } catch (RuntimeException e) {
             //TODO: handle exception
             System.out.println(e);
             throw new InvalidPolynomialSyntax("Error in polynomial format");
@@ -78,14 +76,76 @@ public class Polynomial implements Iterable<Polynomial>, Comparable<Polynomial> 
     }
 
     @Override
-    public Iterator<Polynomial> iterator() {
+    public Iterator<Tuple> iterator() {
         // TODO Auto-generated method stub
-        return null;
+        ArrayList<Tuple> list =new ArrayList<Tuple>();
+        Polynomial.Node temp=this.headNode;
+        while (temp!=null) {
+            list.add(new Tuple(temp.getExponent(), temp.getExponent()));
+            temp=temp.getNextNode();
+        }
+        return list.iterator();
     }
+
+
 
     @Override
     public int compareTo(Polynomial o) {
         // TODO Auto-generated method stub
-        return 0;
+        Polynomial.Node tempOfPolynomial1=this.headNode;
+        Polynomial.Node tempOfPolynomial2=o.getHeadNode();
+        while (tempOfPolynomial1!=null && tempOfPolynomial2!=null) {
+            if (tempOfPolynomial1.getExponent()>tempOfPolynomial2.getExponent()) {
+                return 1;
+            } else if (tempOfPolynomial1.getExponent()<tempOfPolynomial2.getExponent()) {
+                return -1; 
+            }
+            if (tempOfPolynomial1.getCoefficient()>tempOfPolynomial2.getCoefficient()) {
+                return 1;
+            } else if (tempOfPolynomial1.getCoefficient()<tempOfPolynomial2.getCoefficient()) {
+                return -1; 
+            }
+    
+            tempOfPolynomial1=tempOfPolynomial1.getNextNode();
+            tempOfPolynomial2=tempOfPolynomial2.getNextNode();
+        }
+
+
+        if (tempOfPolynomial1==null && tempOfPolynomial2==null) {
+            return 0; 
+        }else if(tempOfPolynomial1==null){
+            return -1;
+        }else{
+            return 1;
+        }
+        
+    }
+
+    @Override
+    public String toString() {
+        String polynomial="";
+        Polynomial.Node temp =this.headNode;
+        while (temp.getNextNode()!=null) {
+            if (temp.getCoefficient()!=0) {
+                if (temp.getExponent()!=1) {
+                    polynomial=polynomial+Double.toString(temp.getCoefficient())+"X^"+Integer.toString(temp.getExponent())+"+";
+                }else{
+                    polynomial=polynomial+Double.toString(temp.getCoefficient())+"X"+"+";
+                }
+            }
+        }
+
+        if (temp.getExponent()!=0) {
+            polynomial=polynomial+Double.toString(temp.getCoefficient())+"X^"+Integer.toString(temp.getExponent());
+        }else{
+            polynomial=polynomial+Double.toString(temp.getCoefficient());
+        }
+        
+        return polynomial;
+    }
+
+    // getter for head node
+    public Polynomial.Node getHeadNode(){
+        return this.headNode;
     }
 }
