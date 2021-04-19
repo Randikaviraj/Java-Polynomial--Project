2b5fcd4fe603f798447f15cb0583c2d8e6ec0a2a
Randikaviraj <randikavirajmax@gmail.com>
2021-04-19 10:14:04 +0530

    add comments on  InvalidPolynomialSyntax class

--- InvalidPolynomialSyntax.java
+++ InvalidPolynomialSyntax.java
@@ -1,7 +1,7 @@
 public class InvalidPolynomialSyntax extends Exception {
 
     /**
-     *
+     *This is custom error class ,which indicates error in polynomial class
      */
     private static final long serialVersionUID = 1L;
 
